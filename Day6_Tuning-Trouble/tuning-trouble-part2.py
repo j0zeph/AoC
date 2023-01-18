92cca53a9c2a918cad85135e852767c00e147351
j0zeph <notaj@tutanota.de>
2023-01-18 08:46:47 +0300

    Add solution for Day 6, Part 2
    For my input, the correct answer was 2980

---
+++ Day6_Tuning-Trouble/tuning-trouble-part2.py
@@ -0,0 +1,37 @@
+from collections import deque
+from typing import Deque
+
+# puzzle_input = 'test-input.txt'
+puzzle_input = 'day6-puzzle-input.txt'
+
+MESSAGE_LENGTH = 14
+
+
+def main():
+    sliding_window = deque()
+    with open(puzzle_input, 'r', encoding='utf-8') as f:
+        while True:
+            char = f.read(1)
+            if not char:  # End of file
+                break
+            else:
+                sliding_window.append(char)
+                if len(sliding_window) == MESSAGE_LENGTH:
+                    if chars_are_unique(sliding_window):
+                        print(f.tell())  # chars processed
+                        break
+                    else:
+                        sliding_window.popleft()
+                else:
+                    continue
+
+
+def chars_are_unique(chars: Deque[str]) -> bool:
+    """Returns True if there are no repeated characters in the inputted deque.
+    Returns False otherwise.
+    """
+    return len(chars) == len(set(chars))
+
+
+if __name__ == '__main__':
+    main()
